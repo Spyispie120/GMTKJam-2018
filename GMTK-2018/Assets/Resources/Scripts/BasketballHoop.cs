7223ab1f6ead4ea01b63483784b39812c773aff0
Nguyen Duong <ducnguyenduchao@gmail.com>
2020-06-28 23:39:54 -0700

    create basketball hoop, and score ui

---
+++ GMTK-2018/Assets/Resources/Scripts/BasketballHoop.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BasketballHoop : MonoBehaviour
+{
+
+    public static int score;
+
+    private Collider2D hoopBlock;
+    private Collider2D hoop;
+
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        hoopBlock = transform.GetChild(1).GetComponent<Collider2D>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.CompareTag("Player"))
+        {
+            Debug.Log("slam dunk");
+            hoopBlock.enabled = false;
+            score++;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            hoopBlock.enabled = true;
+        }
+    }
+}
---
+++ GMTK-2018/Assets/Resources/Scripts/UIScore.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class UIScore : MonoBehaviour
+{
+    public TextMeshProUGUI text;
+
+    void Start()
+    {
+            
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        text.text = "Slam Dunk " + BasketballHoop.score;
+    }
+}
